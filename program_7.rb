6f27679b0397fe0f0d2d9d11fe2fea2f3870cefe
Abdullah Shoukat <abdullahshoukat2222-hash>
2025-12-22 00:38:56 -0800

    updated file

---
+++ program_7.rb
@@ -0,0 +1,10 @@
+print "Enter first number: "
+a = gets.to_f
+
+print "Enter second number: "
+b = gets.to_f
+
+puts "Sum: #{a + b}"
+puts "Difference: #{a - b}"
+puts "Product: #{a * b}"
+puts "Quotient: #{a / b}"
