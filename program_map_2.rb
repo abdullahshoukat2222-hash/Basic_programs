a4d9d3d7ac8d447299f3dfbcf3158acd098ef12f
Abdullah Shoukat <abdullahshoukat2222-hash>
2025-12-28 00:50:15 -0800

    new file

---
+++ program_map_2.rb
@@ -0,0 +1,15 @@
+#result = [1,2,3,4,5,6].map{|n|n*2}
+#puts result
+
+
+
+#result1 = [1,2,3,4,5,6].each do |n|
+#puts n*2
+#end
+
+#puts result1
+result3 = [1,2,3,4,5,6,7,8,9].select do |n|
+t
+end
+
+puts result3
